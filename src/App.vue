eb07017bb2d8503ad0c1e08c6cdb10fdd1b93ae9
Hala-Alobaid <hala.saleh.3152@gmail.com>
2025-07-18 21:20:14 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
 <template>
   <router-view />
 </template>
+<!--
+<style>
+.app {
+  color: rgb(255, 255, 255);
+  color: rgb(204, 216, 206);
+  color: rgb(180, 85, 93);
+  color: rgb(155, 146, 161);
+  color: rgb(161, 184, 190);
+  color: rgb(200, 130, 66);
+  color: rgb(33, 47, 57);
+  color: rgb(220, 185, 168);
+  color: rgb(214, 125, 145);
+  color: rgb(117, 142, 189);
+  color: rgb(167, 178, 163);
+  color: rgb(58, 56, 69);
+}
+
+</style>
+-->
